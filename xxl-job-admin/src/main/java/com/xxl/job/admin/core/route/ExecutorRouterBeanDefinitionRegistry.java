f32c9d787f0acaa6bc75f58b4902aa312366ea4b
KevinClair <kevinclair@apache.org>
2022-10-24 21:21:22 +0800

    bean注入方式修改，取消构造函数引入的方式。

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/route/ExecutorRouterBeanDefinitionRegistry.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/route/ExecutorRouterBeanDefinitionRegistry.java
@@ -1,17 +1,15 @@
 package com.xxl.job.admin.core.route;
 
-import com.xxl.job.admin.core.ExecutorBizRepository;
 import com.xxl.job.admin.core.route.strategy.ExecutorRouteBusyover;
 import com.xxl.job.admin.core.route.strategy.ExecutorRouteFailover;
 import com.xxl.job.admin.core.util.I18nUtil;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 
 /**
  * 执行器路由工厂Bean注册器
@@ -19,11 +17,10 @@ import javax.annotation.Resource;
 @Component
 public class ExecutorRouterBeanDefinitionRegistry implements BeanDefinitionRegistryPostProcessor {
 
-    @Resource
-    private ExecutorBizRepository executorBizRepository;
 
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
+        BeanDefinition executorBizRepository = beanDefinitionRegistry.getBeanDefinition("executorBizRepository");
         BeanDefinitionBuilder executorRouteBusyover = BeanDefinitionBuilder.genericBeanDefinition(ExecutorRouteBusyover.class);
         executorRouteBusyover.addConstructorArgValue(executorBizRepository);
         beanDefinitionRegistry.registerBeanDefinition(I18nUtil.getString("jobconf_route_busyover"), executorRouteBusyover.getBeanDefinition());
