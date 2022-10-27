ec2302f0e8cf71df8f3d4d9455c0bfbc8163e4a9
KevinClair <kevinclair@apache.org>
2022-10-27 21:25:29 +0800

    准备剥离调度的执行任务

---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/service/JobScheduleService.java
@@ -0,0 +1,12 @@
+package com.xxl.job.admin.service;
+
+/**
+ * job执行服务
+ */
+public interface JobScheduleService {
+
+    /**
+     *
+     */
+    void execute();
+}
---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/service/impl/JobScheduleServiceImpl.java
@@ -0,0 +1,15 @@
+package com.xxl.job.admin.service.impl;
+
+import com.xxl.job.admin.service.JobScheduleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobScheduleServiceImpl implements JobScheduleService {
+
+
+
+    @Override
+    public void execute() {
+
+    }
+}
