7e58b9d3c9f2f682522c038de99673ccd41acef3
KevinClair <kevinclair@apache.org>
2022-11-01 21:15:17 +0800

    移动类

---
+++ xxl-job-core/src/main/java/com/xxl/job/core/executor/config/XxlJobConfiguration.java
@@ -0,0 +1,77 @@
+package com.xxl.job.core.executor.config;
+
+/**
+ * xxl-job属性装配
+ */
+public class XxlJobConfiguration {
+
+    private String address;
+
+    private String accessToken;
+
+    private String appName;
+
+    private String ip;
+
+    private int port;
+
+    private String logPath;
+
+    private int logRetentionDays;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getLogPath() {
+        return logPath;
+    }
+
+    public void setLogPath(String logPath) {
+        this.logPath = logPath;
+    }
+
+    public int getLogRetentionDays() {
+        return logRetentionDays;
+    }
+
+    public void setLogRetentionDays(int logRetentionDays) {
+        this.logRetentionDays = logRetentionDays;
+    }
+}
