c09bc0bd3ed7d8410c4742957bdcdc05ddfb8540
KevinClair <704714211@qq.com>
2022-11-01 23:46:33 +0800

    增加bean的注入

--- xxl-job-spring-boot-starter/src/main/java/com/xxl/job/starter/XxlJobAutoConfiguration.java
+++ xxl-job-spring-boot-starter/src/main/java/com/xxl/job/starter/XxlJobAutoConfiguration.java
@@ -1,10 +1,19 @@
 package com.xxl.job.starter;
 
+import com.xxl.job.core.biz.ExecutorBiz;
+import com.xxl.job.core.biz.impl.ExecutorBizImpl;
+import com.xxl.job.core.executor.XxlJobExecutor;
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import com.xxl.job.core.server.EmbedServer;
 import com.xxl.job.core.util.IpUtil;
 import com.xxl.job.core.util.NetUtil;
 import com.xxl.job.starter.config.XxlJobAdminConfiguration;
 import com.xxl.job.core.executor.config.XxlJobConfiguration;
 import com.xxl.job.starter.config.XxlJobExecutorConfiguration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.EnvironmentAware;
@@ -23,6 +32,8 @@ import org.springframework.util.StringUtils;
 @ConditionalOnProperty(prefix = "xxl-job.admin", value = "addresses")
 public class XxlJobAutoConfiguration implements EnvironmentAware {
 
+    private static final Logger logger = LoggerFactory.getLogger(XxlJobAutoConfiguration.class);
+
     private ConfigurableEnvironment environment;
 
     @Bean
@@ -30,8 +41,11 @@ public class XxlJobAutoConfiguration implements EnvironmentAware {
     public XxlJobConfiguration xxlJobExecutor(@NonNull XxlJobAdminConfiguration adminConfiguration, @NonNull XxlJobExecutorConfiguration executorConfiguration) {
         XxlJobConfiguration configuration = new XxlJobConfiguration();
         configuration.setAddress(adminConfiguration.getAddress());
-        configuration.setAccessToken(adminConfiguration.getAccessToken());
-
+        if (!StringUtils.hasLength(adminConfiguration.getAccessToken())){
+            logger.warn("xxl-job accessToken is empty. To ensure system security, please set the accessToken.");
+        }else {
+            configuration.setAccessToken(adminConfiguration.getAccessToken());
+        }
         if (!StringUtils.hasLength(executorConfiguration.getAppName())) {
             String applicationName = environment.getProperty("spring.application.name");
             if (!StringUtils.hasLength(applicationName)) {
@@ -51,6 +65,23 @@ public class XxlJobAutoConfiguration implements EnvironmentAware {
         configuration.setLogPath(executorConfiguration.getLogPath());
         configuration.setLogRetentionDays(executorConfiguration.getLogRetentionDays());
         return configuration;
+    }
+
+    @Bean
+    public XxlJobExecutor xxlJobExecutor(XxlJobConfiguration configuration){
+        return new XxlJobSpringExecutor(configuration);
+    }
+
+    @Bean
+    @ConditionalOnBean(value = XxlJobExecutor.class)
+    public ExecutorBiz executorBiz(){
+        return new ExecutorBizImpl();
+    }
+
+    @Bean
+    @ConditionalOnBean(value = XxlJobExecutor.class)
+    public EmbedServer embedServer(ExecutorBiz executorBiz, XxlJobConfiguration configuration){
+        return new EmbedServer(executorBiz, configuration);
     }
 
     @Override
