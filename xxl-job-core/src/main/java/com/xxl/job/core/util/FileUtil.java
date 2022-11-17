eeb8c5f9ae862316ef06b13692c40cf6acf21464
KevinClair <704714211@qq.com>
2022-11-17 18:14:28 +0800

    资源关闭

--- xxl-job-core/src/main/java/com/xxl/job/core/util/FileUtil.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/util/FileUtil.java
@@ -56,126 +56,24 @@ public class FileUtil {
         }
 
         // append file content
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(file);
+        try(FileOutputStream fos= new FileOutputStream(file)) {
             fos.write(data);
             fos.flush();
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    logger.error(e.getMessage(), e);
-                }
-            }
         }
-
     }
 
     public static byte[] readFileContent(File file) {
         Long filelength = file.length();
         byte[] filecontent = new byte[filelength.intValue()];
 
-        FileInputStream in = null;
-        try {
-            in = new FileInputStream(file);
+        try(FileInputStream in = new FileInputStream(file)) {
             in.read(filecontent);
-            in.close();
-
             return filecontent;
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             return null;
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    logger.error(e.getMessage(), e);
-                }
-            }
         }
     }
-
-
-    /*public static void appendFileLine(String fileName, String content) {
-
-        // file
-        File file = new File(fileName);
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                logger.error(e.getMessage(), e);
-                return;
-            }
-        }
-
-        // content
-        if (content == null) {
-            content = "";
-        }
-        content += "\r\n";
-
-        // append file content
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(file, true);
-            fos.write(content.getBytes("utf-8"));
-            fos.flush();
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    logger.error(e.getMessage(), e);
-                }
-            }
-        }
-
-    }
-
-    public static List<String> loadFileLines(String fileName){
-
-        List<String> result = new ArrayList<>();
-
-        // valid log file
-        File file = new File(fileName);
-        if (!file.exists()) {
-            return result;
-        }
-
-        // read file
-        StringBuffer logContentBuffer = new StringBuffer();
-        int toLineNum = 0;
-        LineNumberReader reader = null;
-        try {
-            //reader = new LineNumberReader(new FileReader(logFile));
-            reader = new LineNumberReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
-            String line = null;
-            while ((line = reader.readLine())!=null) {
-                if (line!=null && line.trim().length()>0) {
-                    result.add(line);
-                }
-            }
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    logger.error(e.getMessage(), e);
-                }
-            }
-        }
-
-        return result;
-    }*/
-
 }
