3de62cb4fd9cb3993b29547387d1e98707d0601d
KevinClair <kevinclair@apache.org>
2023-01-18 18:04:26 +0800

    属性初始化

--- xxl-job-core/src/main/java/com/xxl/job/core/executor/AdminBizClientManager.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/executor/AdminBizClientManager.java
@@ -7,31 +7,18 @@ import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Admin客户端管理
  */
 public class AdminBizClientManager {
 
-    private List<AdminBiz> adminBizList;
+    private final List<AdminBiz> adminBizList = new ArrayList<AdminBiz>();
 
     public AdminBizClientManager(XxlJobConfiguration configuration) {
-        this.initClients(configuration);
-    }
-
-    /**
-     * 初始化admin客户端地址
-     *
-     * @param configuration 配置数据
-     */
-    public void initClients(XxlJobConfiguration configuration) {
         for (String address : configuration.getAddress().trim().split(",")) {
             if (StringUtils.hasLength(address)) {
                 AdminBiz adminBiz = new AdminBizClient(address.trim(), configuration.getAccessToken());
-                if (Objects.isNull(adminBizList)) {
-                    adminBizList = new ArrayList<AdminBiz>();
-                }
                 adminBizList.add(adminBiz);
             }
         }
