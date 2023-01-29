1a0ed33a83c064763fd8d40126d89eb0cc9e029b
KevinClair <kevinclair@apache.org>
2023-01-29 12:36:53 +0800

    增加字段唯一性

--- doc/db/tables_xxl_job.sql
+++ doc/db/tables_xxl_job.sql
@@ -8,31 +8,32 @@ use `xxl_job`;
 SET NAMES utf8mb4;
 
 CREATE TABLE `xxl_job_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `job_group` int(11) NOT NULL COMMENT '执行器主键ID',
-  `job_desc` varchar(255) NOT NULL,
-  `add_time` datetime DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL,
-  `author` varchar(64) DEFAULT NULL COMMENT '作者',
-  `alarm_email` varchar(255) DEFAULT NULL COMMENT '报警邮件',
-  `schedule_type` varchar(50) NOT NULL DEFAULT 'NONE' COMMENT '调度类型',
-  `schedule_conf` varchar(128) DEFAULT NULL COMMENT '调度配置，值含义取决于调度类型',
-  `misfire_strategy` varchar(50) NOT NULL DEFAULT 'DO_NOTHING' COMMENT '调度过期策略',
-  `executor_route_strategy` varchar(50) DEFAULT NULL COMMENT '执行器路由策略',
-  `executor_handler` varchar(255) DEFAULT NULL COMMENT '执行器任务handler',
-  `executor_param` varchar(512) DEFAULT NULL COMMENT '执行器任务参数',
-  `executor_block_strategy` varchar(50) DEFAULT NULL COMMENT '阻塞处理策略',
-  `executor_timeout` int(11) NOT NULL DEFAULT '0' COMMENT '任务执行超时时间，单位秒',
-  `executor_fail_retry_count` int(11) NOT NULL DEFAULT '0' COMMENT '失败重试次数',
-  `glue_type` varchar(50) NOT NULL COMMENT 'GLUE类型',
-  `glue_source` mediumtext COMMENT 'GLUE源代码',
-  `glue_remark` varchar(128) DEFAULT NULL COMMENT 'GLUE备注',
-  `glue_updatetime` datetime DEFAULT NULL COMMENT 'GLUE更新时间',
-  `child_jobid` varchar(255) DEFAULT NULL COMMENT '子任务ID，多个逗号分隔',
-  `trigger_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '调度状态：0-停止，1-运行',
-  `trigger_last_time` bigint(13) NOT NULL DEFAULT '0' COMMENT '上次调度时间',
-  `trigger_next_time` bigint(13) NOT NULL DEFAULT '0' COMMENT '下次调度时间',
-  PRIMARY KEY (`id`)
+                                `id` int(11) NOT NULL AUTO_INCREMENT,
+                                `job_group` int(11) NOT NULL COMMENT '执行器主键ID',
+                                `job_desc` varchar(255) NOT NULL,
+                                `add_time` datetime DEFAULT NULL,
+                                `update_time`               datetime DEFAULT NULL,
+                                `author`                    varchar(64) DEFAULT NULL COMMENT '作者',
+                                `alarm_email`               varchar(255) DEFAULT NULL COMMENT '报警邮件',
+                                `schedule_type`             varchar(50) NOT NULL DEFAULT 'NONE' COMMENT '调度类型',
+                                `schedule_conf`             varchar(128) DEFAULT NULL COMMENT '调度配置，值含义取决于调度类型',
+                                `misfire_strategy`          varchar(50) NOT NULL DEFAULT 'DO_NOTHING' COMMENT '调度过期策略',
+                                `executor_route_strategy`   varchar(50) DEFAULT NULL COMMENT '执行器路由策略',
+                                `executor_handler`          varchar(255) DEFAULT NULL COMMENT '执行器任务handler',
+                                `executor_param`            varchar(512) DEFAULT NULL COMMENT '执行器任务参数',
+                                `executor_block_strategy`   varchar(50) DEFAULT NULL COMMENT '阻塞处理策略',
+                                `executor_timeout`          int(11) NOT NULL DEFAULT '0' COMMENT '任务执行超时时间，单位秒',
+                                `executor_fail_retry_count` int(11) NOT NULL DEFAULT '0' COMMENT '失败重试次数',
+                                `glue_type`                 varchar(50) NOT NULL COMMENT 'GLUE类型',
+                                `glue_source`               mediumtext COMMENT 'GLUE源代码',
+                                `glue_remark`               varchar(128) DEFAULT NULL COMMENT 'GLUE备注',
+                                `glue_updatetime`           datetime DEFAULT NULL COMMENT 'GLUE更新时间',
+                                `child_jobid`               varchar(255) DEFAULT NULL COMMENT '子任务ID，多个逗号分隔',
+                                `trigger_status`            tinyint(4) NOT NULL DEFAULT '0' COMMENT '调度状态：0-停止，1-运行',
+                                `trigger_last_time`         bigint(13) NOT NULL DEFAULT '0' COMMENT '上次调度时间',
+                                `trigger_next_time`         bigint(13) NOT NULL DEFAULT '0' COMMENT '下次调度时间',
+                                PRIMARY KEY (`id`),
+                                UNIQUE KEY `UNIQUE_job_desc` (`job_desc`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `xxl_job_log` (
@@ -88,14 +89,16 @@ CREATE TABLE `xxl_job_registry` (
   KEY `i_g_k_v` (`registry_group`,`registry_key`,`registry_value`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `xxl_job_group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `app_name` varchar(64) NOT NULL COMMENT '执行器AppName',
-  `title` varchar(12) NOT NULL COMMENT '执行器名称',
-  `address_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '执行器地址类型：0=自动注册、1=手动录入',
-  `address_list` text COMMENT '执行器地址列表，多地址逗号分隔',
-  `update_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `xxl_job_group`
+(
+    `id`           int(11) NOT NULL AUTO_INCREMENT,
+    `app_name`     varchar(64) NOT NULL COMMENT '执行器AppName',
+    `title`        varchar(12) NOT NULL COMMENT '执行器名称',
+    `address_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '执行器地址类型：0=自动注册、1=手动录入',
+    `address_list` text COMMENT '执行器地址列表，多地址逗号分隔',
+    `update_time`  datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `UNIQUE_app_name` (`app_name`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `xxl_job_user` (
