b77b8737a427e46664b25a9666dc8f46840903cf
KevinClair <kevinclair@apache.org>
2023-02-06 21:25:58 +0800

    完善逻辑

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/thread/JobRegistryHelper.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/thread/JobRegistryHelper.java
@@ -35,7 +35,7 @@ public class JobRegistryHelper {
 
     private final ExecutorManagerClientRepository executorManagerClientRepository;
 
-    private final ThreadPoolExecutor registryOrRemoveThreadPool;
+    private final ThreadPoolExecutor heartBeatCheckThreadPool;
 
     private final ScheduledThreadPoolExecutor heartBeatThreadPool;
 
@@ -50,7 +50,7 @@ public class JobRegistryHelper {
         this.jobRegistryDao = jobRegistryDao;
         this.executorManagerClientRepository = executorManagerClientRepository;
         // for registry or remove TODO 请求Netty服务器，保持心跳
-        this.registryOrRemoveThreadPool = new ThreadPoolExecutor(
+        this.heartBeatCheckThreadPool = new ThreadPoolExecutor(
                 2,
                 10,
                 30L,
@@ -66,130 +66,120 @@ public class JobRegistryHelper {
 
     public void start() {
         // 心跳检测
-        heartBeatThreadPool.scheduleAtFixedRate(() -> {
-            try {
-                // auto registry group
-//                List<XxlJobGroup> groupList = jobGroupDao.findByAddressType(0);
-//                if (!CollectionUtils.isEmpty(groupList)) {
-
-                // remove dead address (admin/executor)
-                // 删除最后活跃时间大于90秒的节点
-                List<XxlJobRegistry> deadRegistry = jobRegistryDao.findDead(RegistryConstants.DEAD_TIMEOUT, new Date());
-                List<Integer> deadIds = new ArrayList<>();
-                for (XxlJobRegistry each : deadRegistry) {
-                    deadIds.add(each.getId());
-                    putToZombie(each);
-                }
-                jobRegistryDao.removeDead(deadIds);
-
-                // fresh online address (admin/executor)
-                // 存储所有仍存活的节点
-                HashMap<String, List<String>> appAddressMap = new HashMap<>();
-                // 心跳成功的数据
-                List<Integer> beatSuccessRegistryIds = new ArrayList<>();
-                // 查询所有存活的节点
-                List<XxlJobRegistry> list = jobRegistryDao.findAll(RegistryConstants.DEAD_TIMEOUT, new Date());
-                list.stream()
-                        .forEach(each -> {
-                            // TODO 改为多线程
-                            String appName = each.getRegistryKey();
-                            ReturnT<String> beatResult = executorManagerClientRepository.getExecutorManagerClient(each.getRegistryValue()).beat();
-                            if (beatResult.getCode() == ReturnT.SUCCESS_CODE) {
-                                // 心跳成功
-                                List<String> registryList = appAddressMap.getOrDefault(appName, new ArrayList<>());
-                                if (!registryList.contains(each.getRegistryValue())) {
-                                    registryList.add(each.getRegistryValue());
-                                }
-                                appAddressMap.put(appName, registryList);
-                                beatSuccessRegistryIds.add(each.getId());
-                            } else {
-                                // 心跳失败，加入到ZOMBIE_REGISTRY_MAP
-                                putToZombie(each);
-                            }
-                        });
-
-                // 更新所有心跳成功的节点
-                Date current = new Date();
-                if (!CollectionUtils.isEmpty(beatSuccessRegistryIds)) {
-                    jobRegistryDao.beatUpdate(beatSuccessRegistryIds, current);
-                }
-                // fresh group address
-                for (Map.Entry<String, List<String>> entry : appAddressMap.entrySet()) {
-                    XxlJobGroup xxlJobGroup = jobGroupDao.selectByAppName(entry.getKey());
-                    if (Objects.isNull(xxlJobGroup)) {
-                        continue;
-                    }
-                    // TODO 这里为啥需要排序
-                    List<String> addressList = entry.getValue();
-                    Collections.sort(addressList);
-                    xxlJobGroup.setAddressList(addressList.stream().collect(Collectors.joining(",")));
-                    xxlJobGroup.setUpdateTime(current);
-                    jobGroupDao.update(xxlJobGroup);
-                }
-            } catch (Exception e) {
-                logger.error("Admin心跳检测异常", e);
-            }
-        }, 0, RegistryConstants.BEAT_TIMEOUT, TimeUnit.SECONDS);
+        heartBeatThreadPool.scheduleAtFixedRate(() -> heartBeatSchedule(), 0, RegistryConstants.BEAT_TIMEOUT, TimeUnit.SECONDS);
 
         // 僵尸节点检测
-        heartBeatThreadPool.scheduleAtFixedRate(() -> {
-            Date current = new Date();
-            for (Map.Entry<String, List<XxlJobRegistry>> entry : ZOMBIE_REGISTRY_MAP.entrySet()) {
-                // 开始心跳检查
-                List<XxlJobRegistry> xxlJobRegistryList = entry.getValue();
-                // 存储所有心跳成功的僵尸节点
-                HashMap<String, List<String>> appAddressMap = new HashMap<>();
-                // 心跳成功的数据
-                List<Integer> beatSuccessRegistryIds = new ArrayList<>();
-                Iterator<XxlJobRegistry> iterator = xxlJobRegistryList.iterator();
-                while (iterator.hasNext()) {
-                    XxlJobRegistry each = iterator.next();
-                    // 如果最后一次的心跳时间距离当前时间已经超过两个ZOMBIE_DEAD_TIMEOUT，删除当前节点
-                    Boolean exceed = DateUtil.exceed(each.getUpdateTime(), current, RegistryConstants.ZOMBIE_DEAD_TIMEOUT * 1000);
-                    if (exceed) {
-                        xxlJobRegistryList.remove(each);
-                        // 删除客户端
-                        executorManagerClientRepository.remove(each.getRegistryValue());
-                        continue;
-                    }
+        heartBeatThreadPool.scheduleAtFixedRate(() -> zombieSchedule(), RegistryConstants.BEAT_TIMEOUT, RegistryConstants.BEAT_TIMEOUT, TimeUnit.SECONDS);
+    }
 
-                    ReturnT<String> beatResult = executorManagerClientRepository.getExecutorManagerClient(each.getRegistryValue()).beat();
-                    if (beatResult.getCode() == ReturnT.SUCCESS_CODE) {
-                        // 心跳成功
-                        List<String> registryList = appAddressMap.getOrDefault(entry.getKey(), new ArrayList<>());
-                        if (!registryList.contains(each.getRegistryValue())) {
-                            registryList.add(each.getRegistryValue());
-                        }
-                        appAddressMap.put(entry.getKey(), registryList);
-                        beatSuccessRegistryIds.add(each.getId());
-                    }
-                }
-                if (!CollectionUtils.isEmpty(beatSuccessRegistryIds)) {
-                    jobRegistryDao.beatUpdate(beatSuccessRegistryIds, current);
-                }
-                // fresh group address
-                for (Map.Entry<String, List<String>> entryMap : appAddressMap.entrySet()) {
-                    // TODO 这里为啥需要排序 这里理论上应该按照最后一次心跳时间排序，把最近的一次心跳的address放在最前面，这样确保负载均衡时的成功几率
-                    List<String> addressList = entryMap.getValue();
-                    // TODO addressList可能是空集合吗？
-                    Collections.sort(addressList);
-                    String addressString = addressList.stream().collect(Collectors.joining(","));
-                    XxlJobGroup xxlJobGroup = jobGroupDao.selectByAppName(entryMap.getKey());
-                    if (Objects.isNull(xxlJobGroup)) {
-                        // 如果xxlJobGroup不存在，跳过
-                        continue;
-                    }
-                    if (StringUtils.hasText(xxlJobGroup.getAddressList())) {
-                        xxlJobGroup.setAddressList(xxlJobGroup.getAddressList() + "," + addressString);
-                    } else {
-                        xxlJobGroup.setAddressList(addressString);
-                    }
-                    xxlJobGroup.setUpdateTime(current);
-                    jobGroupDao.update(xxlJobGroup);
-                }
-
+    private void zombieSchedule() {
+        Date current = new Date();
+        for (Map.Entry<String, List<XxlJobRegistry>> entry : ZOMBIE_REGISTRY_MAP.entrySet()) {
+            XxlJobGroup xxlJobGroup = jobGroupDao.selectByAppName(entry.getKey());
+            if (Objects.isNull(xxlJobGroup)) {
+                // 如果xxlJobGroup不存在，删除ZOMBIE_REGISTRY_MAP中的值，不执行后续的流程
+                ZOMBIE_REGISTRY_MAP.remove(entry.getKey());
+                continue;
             }
-        }, RegistryConstants.BEAT_TIMEOUT, RegistryConstants.BEAT_TIMEOUT, TimeUnit.SECONDS);
+            // 开始心跳检查
+            List<XxlJobRegistry> xxlJobRegistryList = entry.getValue();
+            // 心跳成功的数据
+            List<XxlJobRegistry> beatSuccessRegistry = new ArrayList<>();
+            List<String> addressList = new ArrayList<>();
+            Iterator<XxlJobRegistry> iterator = xxlJobRegistryList.iterator();
+            while (iterator.hasNext()) {
+                XxlJobRegistry each = iterator.next();
+                // 如果最后一次的心跳时间距离当前时间已经超过两个ZOMBIE_DEAD_TIMEOUT，删除当前节点
+                Boolean exceed = DateUtil.exceed(each.getUpdateTime(), current, RegistryConstants.ZOMBIE_DEAD_TIMEOUT * 1000);
+                if (exceed) {
+                    iterator.remove();
+                    // 删除客户端
+                    executorManagerClientRepository.remove(each.getRegistryValue());
+                    continue;
+                }
+                ReturnT<String> beatResult = executorManagerClientRepository.getExecutorManagerClient(each.getRegistryValue()).beat();
+                if (beatResult.getCode() == ReturnT.SUCCESS_CODE) {
+                    // 心跳成功
+                    each.setUpdateTime(current);
+                    beatSuccessRegistry.add(each);
+                    addressList.add(each.getRegistryValue());
+                    // 从iterator中删除
+                    iterator.remove();
+                }
+            }
+            if (CollectionUtils.isEmpty(xxlJobRegistryList)) {
+                ZOMBIE_REGISTRY_MAP.remove(entry.getKey());
+            }
+            if (!CollectionUtils.isEmpty(beatSuccessRegistry)) {
+                jobRegistryDao.saveBatch(beatSuccessRegistry);
+                // fresh group address
+                String addressString = addressList.stream().collect(Collectors.joining(","));
+                if (StringUtils.hasText(xxlJobGroup.getAddressList())) {
+                    xxlJobGroup.setAddressList(xxlJobGroup.getAddressList() + "," + addressString);
+                } else {
+                    xxlJobGroup.setAddressList(addressString);
+                }
+                xxlJobGroup.setUpdateTime(current);
+                jobGroupDao.update(xxlJobGroup);
+            }
+        }
+    }
+
+    private void heartBeatSchedule() {
+        try {
+            // remove dead address (admin/executor)
+            // 删除最后活跃时间大于90秒的节点
+            List<XxlJobRegistry> deadRegistry = jobRegistryDao.findDead(RegistryConstants.DEAD_TIMEOUT, new Date());
+            List<Integer> deadIds = new ArrayList<>();
+            for (XxlJobRegistry each : deadRegistry) {
+                deadIds.add(each.getId());
+                putToZombie(each);
+            }
+            jobRegistryDao.removeDead(deadIds);
+
+            // fresh online address (admin/executor)
+            // 存储所有仍存活的节点
+            ConcurrentHashMap<String, List<String>> appAddressMap = new ConcurrentHashMap<>();
+            // 心跳成功的数据
+            List<Integer> beatSuccessRegistryIds = new CopyOnWriteArrayList<>();
+            // 查询所有存活的节点
+            List<XxlJobRegistry> list = jobRegistryDao.findAll(RegistryConstants.DEAD_TIMEOUT, new Date());
+            CompletableFuture[] completableFutures = list.stream().map(each -> CompletableFuture.runAsync(() -> {
+                String appName = each.getRegistryKey();
+                ReturnT<String> beatResult = executorManagerClientRepository.getExecutorManagerClient(each.getRegistryValue()).beat();
+                if (beatResult.getCode() == ReturnT.SUCCESS_CODE) {
+                    // 心跳成功
+                    List<String> registryList = appAddressMap.computeIfAbsent(appName, k -> new ArrayList<>());
+                    if (!registryList.contains(each.getRegistryValue())) {
+                        registryList.add(each.getRegistryValue());
+                    }
+                    appAddressMap.put(appName, registryList);
+                    beatSuccessRegistryIds.add(each.getId());
+                } else {
+                    // 心跳失败，加入到ZOMBIE_REGISTRY_MAP
+                    putToZombie(each);
+                }
+            }, heartBeatCheckThreadPool)).toArray(array -> new CompletableFuture[list.size()]);
+            CompletableFuture.allOf(completableFutures);
+            // 更新所有心跳成功的节点
+            Date current = new Date();
+            if (!CollectionUtils.isEmpty(beatSuccessRegistryIds)) {
+                jobRegistryDao.beatUpdate(beatSuccessRegistryIds, current);
+            }
+            // fresh group address
+            for (Map.Entry<String, List<String>> entry : appAddressMap.entrySet()) {
+                XxlJobGroup xxlJobGroup = jobGroupDao.selectByAppName(entry.getKey());
+                if (Objects.isNull(xxlJobGroup)) {
+                    continue;
+                }
+                List<String> addressList = entry.getValue();
+                xxlJobGroup.setAddressList(addressList.stream().collect(Collectors.joining(",")));
+                xxlJobGroup.setUpdateTime(current);
+                jobGroupDao.update(xxlJobGroup);
+            }
+        } catch (Exception e) {
+            logger.error("Admin心跳检测异常", e);
+        }
     }
 
     private void putToZombie(XxlJobRegistry registry) {
@@ -202,7 +192,7 @@ public class JobRegistryHelper {
 
     public void toStop() {
         // stop registryOrRemoveThreadPool
-        ThreadPoolExecutorUtil.gracefulShutdown(registryOrRemoveThreadPool, 1000, ADMIN_REGISTRY_OR_REMOVE);
+        ThreadPoolExecutorUtil.gracefulShutdown(heartBeatCheckThreadPool, 1000, ADMIN_REGISTRY_OR_REMOVE);
 
         // stop heartBeatThreadPool
         ThreadPoolExecutorUtil.gracefulShutdown(heartBeatThreadPool, 3000, ADMIN_HEARTBEAT_CHECK);
